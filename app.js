d86b1b486cb028f2766a1b9f3fc5e68105dc0445
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2015-07-10 02:16:11 +0200

    drawer somewhat usable

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ window.addEventListener("load", function() {
   console.log("Hello World!");
   // localStorage.theme = "communications";
 });
+
+var header = document.querySelector("gaia-header");
+var drawer = document.querySelector("gaia-drawer");
+header.addEventListener('action', function() {
+  drawer.toggle();
+});
