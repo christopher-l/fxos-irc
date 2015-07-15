2ed43670eac919371239ad96fe8354ab8627e171
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2015-07-15 16:51:16 +0200

    hide user icon when drawer is open

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-window.addEventListener("load", function() {
-  console.log("Hello World!");
+window.addEventListener('load', function() {
+  console.log('Hello World!');
   // localStorage.theme = "communications";
 });
 
-var header = document.querySelector("gaia-header");
-var drawer = document.querySelector("gaia-drawer");
+var header = document.querySelector('gaia-header');
+var drawer = document.querySelector('gaia-drawer');
 header.addEventListener('action', function() {
   drawer.toggle();
+});
+
+drawer.addEventListener('DOMAttrModified', function (event) {
+  if (event.attrName !== 'open') { return; }
+  var button = header.querySelector('button');
+  if (event.attrChange === 2) { // open drawer
+    // button.style.visibility = 'hidden';
+    button.style.opacity = 0;
+  } else if (event.attrChange === 3) { // close drawer
+    // button.style.visibility = 'visible';
+    button.style.opacity = 100;
+  }
 });
