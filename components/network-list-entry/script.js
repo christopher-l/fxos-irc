085bd612ce8bf4e7bc44086dd63c39927bf4fe37
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2015-07-26 17:27:43 +0200

    collapse halfway done

--- components/network-list-entry/script.js
+++ components/network-list-entry/script.js
@@ -8,6 +8,13 @@ var baseUrl = '/components/network-list-entry/';
 proto.createdCallback = function() {
   var tmpl = template.content.cloneNode(true);
   var shadow = this.createShadowRoot();
+
+  this.els = {};
+  this.els.inner = tmpl.querySelector('.inner');
+  this.els.channelList = tmpl.querySelector('.channel-list');
+  this.els.channelWrapper = tmpl.querySelector('.channel-wrapper');
+  this.collapse();
+
   shadow.appendChild(tmpl);
 
   var style = document.createElement('style');
@@ -16,16 +23,47 @@ proto.createdCallback = function() {
   shadow.appendChild(style);
 };
 
+proto.attributeChangedCallback = function(attr, oldVal, newVal) {
+  if (attr == 'collapsed') {
+    this.collapse(newVal !== null);
+  }
+};
+
+proto.collapse = function(value) {
+  if (value == false) { return this.expand(); }
+  var height = this.els.channelWrapper.clientHeight;
+  this.els.channelList.style.height = '0px';
+  this.els.channelList.style.zIndex = '0';
+  this.els.channelList.style.transform = 'translateY(-' + height + 'px)';
+  this.els.inner.setAttribute('collapsed', '');
+  if (!this.hasAttribute('collapsed')) { this.setAttribute('collapsed', ''); }
+};
+
+proto.expand = function() {
+  var height = this.els.channelWrapper.clientHeight;
+  this.els.channelList.style.height = height + 'px';
+  this.els.channelList.style.zIndex = '';
+  this.els.channelList.style.transform = '';
+  this.els.inner.removeAttribute('collapsed');
+  if (this.hasAttribute('collapsed')) { this.removeAttribute('collapsed'); }
+};
+
+proto.toggle = function () {
+  this.collapse(!this.hasAttribute('collapsed'));
+};
+
 var template = document.createElement('template');
 template.innerHTML = `
   <div class="inner">
-    <span class="collapse-indicator">&#9660;</span>
     <p class="network">
+      <span class="collapse-indicator">&#9660;</span>
       <span class="network-name"><content select="span"></content></span>
       <span class="counter"></span>
     </p>
     <div class="channel-list">
-      <content></content>
+      <div class="channel-wrapper">
+        <content></content>
+      </div>
     </div>
   </div>
 `;
