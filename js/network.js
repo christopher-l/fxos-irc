22d08e6f99ae9a3cd67fa7e250f0a5de93cbaef7
Christopher Luebbemeier <christopher.luebbemeier@uni-weimar.de>
2015-08-11 23:57:23 +0200

    network dialog: change title on input

--- js/network.js
+++ js/network.js
@@ -18,55 +18,61 @@ Network.prototype.openConfig = function() {
       config.close();
     });
   }
-  config.innerHTML = `
-    <gaia-list>
-      <li>
-        <div flex>
-          <h3>Name</h3>
-          <gaia-text-input id="name" placeholder="e.g. Freenode" required></gaia-text-input>
-        </div>
-      </li>
-      <li>
-        <div flex>
-          <h3>URL</h3>
-          <gaia-text-input id="url" placeholder="e.g. irc.freenode.net" required></gaia-text-input>
-        </div>
-      </li>
-      <li flexbox>
-        <label flex>
-          <h3>Port</h3>
-          <!-- <p>Leave empty for default</p> -->
-        </label>
-        <gaia-text-input id="port" type="number" maxlength="5" placeholder="6667"></gaia-text-input>
-      </li>
-      <li class="ripple">
-        <label flex for="tls">
-          <h3>Use TLS</h3>
-        </label>
-        <gaia-checkbox id="tls"></gaia-checkbox>
-      </li>
-      <li class="ripple">
-        <label flex for="auto-connect">
-          <h3>Auto Connect</h3>
-        </label>
-        <gaia-checkbox id="auto-connect"></gaia-checkbox>
-      </li>
-    </gaia-list>
-    <style>
-      h3, p {
-        padding-left: 1rem;
-      }
-      gaia-text-input {
-        margin: .5rem 0px !important;
-      }
-      #port {
-        width: 5rem !important;
-        margin: 0px !important;
-      }
-    </style>
-  `;
+  config.innerHTML = CONFIG_HTML;
   document.body.appendChild(config);
+  var nameInput = config.querySelector('#name');
+  nameInput.els.input.addEventListener('input', function() {
+    config.title = nameInput.value;
+  });
 };
+
+const CONFIG_HTML = `
+  <gaia-list>
+    <li>
+      <div flex>
+        <h3>Name</h3>
+        <gaia-text-input id="name" placeholder="e.g. Freenode" required></gaia-text-input>
+      </div>
+    </li>
+    <li>
+      <div flex>
+        <h3>URL</h3>
+        <gaia-text-input id="url" placeholder="e.g. irc.freenode.net" required></gaia-text-input>
+      </div>
+    </li>
+    <li flexbox>
+      <label flex>
+        <h3>Port</h3>
+        <!-- <p>Leave empty for default</p> -->
+      </label>
+      <gaia-text-input id="port" type="number" maxlength="5" placeholder="6667"></gaia-text-input>
+    </li>
+    <li class="ripple">
+      <label flex for="tls">
+        <h3>Use TLS</h3>
+      </label>
+      <gaia-checkbox id="tls"></gaia-checkbox>
+    </li>
+    <li class="ripple">
+      <label flex for="auto-connect">
+        <h3>Auto Connect</h3>
+      </label>
+      <gaia-checkbox id="auto-connect"></gaia-checkbox>
+    </li>
+  </gaia-list>
+  <style>
+    h3, p {
+      padding-left: 1rem;
+    }
+    gaia-text-input {
+      margin: .5rem 0px !important;
+    }
+    #port {
+      width: 5rem !important;
+      margin: 0px !important;
+    }
+  </style>
+`;
 
 module.exports = Network;
 
