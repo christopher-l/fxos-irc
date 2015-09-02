c4499f79989841e99e92d38408cff914a2f262f8
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2015-09-03 01:08:22 +0200

    irc-open directive one way

--- app/js/directives.js
+++ app/js/directives.js
@@ -12,3 +12,24 @@ irc.directive('ircAction', function() {
     }
   };
 });
+
+irc.directive('ircOpen', function() {
+  return {
+    restrict: 'A',
+    priority: 100,
+    link: function(scope, element, attrs) {
+      scope.$watch(attrs.ircOpen, function(value) {
+        if (value) {
+          element[0].setAttribute('open', '');
+        } else {
+          element[0].removeAttribute('open');
+        }
+      });
+      // scope.$watch(function() {
+      //   return element.attr('open');
+      // }, function(value) {
+      //   attrs.$set('ircOpen', value);
+      // });
+    }
+  };
+});
