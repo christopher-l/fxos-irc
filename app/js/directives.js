8b7f655e3dbc84d2b22ba423e79dfb84612027e1
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2015-09-04 02:19:21 +0200

    collapse network entries

--- app/js/directives.js
+++ app/js/directives.js
@@ -13,12 +13,13 @@ irc.directive('ircAction', function() {
   };
 });
 
-irc.directive('ircOpen', function() {
+irc.directive('ircOpen', ['$parse', function($parse) {
   return {
     restrict: 'A',
     priority: 100,
     link: function(scope, element, attrs) {
-      scope.$watch(attrs.ircOpen, function(value) {
+      var model = $parse(attrs.ircOpen);
+      scope.$watch(model, function(value) {
         if (value) {
           element[0].setAttribute('open', '');
         } else {
@@ -28,7 +29,7 @@ irc.directive('ircOpen', function() {
       scope.$watch(function() {
         return element[0].hasAttribute('open');
       }, function(value) {
-        scope[attrs.ircOpen] = value;
+        model.assign(scope, value);
       });
       new MutationObserver(function(mutations) {
         mutations.forEach(function(mutation) {
@@ -40,18 +41,18 @@ irc.directive('ircOpen', function() {
       }).observe(element[0], {attributes: true});
     }
   };
-});
+}]);
 
-irc.directive('ircClientHeight', function($log) {
+irc.directive('ircClientHeight', ['$parse', function($parse) {
   return {
     restrict: 'A',
     link: function(scope, element, attrs) {
+      var model = $parse(attrs.ircClientHeight);
       scope.$watch(function() {
         return element.prop('clientHeight');
       }, function(value) {
-        scope[attrs.ircClientHeight] = value;
-        $log.log(value);
+        model.assign(scope, value);
       });
     }
   };
-});
+}]);
