2c88cc65e18ddf8f56ae7eed80413e6251c27fd6
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2015-09-07 13:33:09 +0200

    minor fix

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -5,7 +5,6 @@ module.exports = function(config){
 
     files : [
       'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-animate/angular-animate.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
       'app/js/*.js',
       'test/unit/*.test.js',
