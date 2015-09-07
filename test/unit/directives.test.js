e5fc762032b6821da2a0786a3b347a1f0c41a2f8
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2015-09-07 18:47:26 +0200

    minor change in unit tests

--- test/unit/directives.test.js
+++ test/unit/directives.test.js
@@ -6,9 +6,9 @@ describe('irc-theme-group', function() {
 
   var scope;
   var head;
-  var themeGroup;
-  var themeColor;
-  var notThemeColor;
+  var onThemeGroupRemove;
+  var onThemeColorRemove;
+  var onNotThemeColorRemove;
 
   var updateTheme = function() {
     scope.theme = 'new-theme';
@@ -18,41 +18,36 @@ describe('irc-theme-group', function() {
   beforeEach(inject(function($compile, $rootScope) {
     scope = $rootScope.$new();
     head = $compile('<head></head>')(scope);
-    themeGroup = $compile('<meta content="{{theme}}" irc-theme-group>')(scope);
-    themeColor = $compile('<meta name="theme-color">')(scope);
-    notThemeColor = $compile('<meta name="not-theme-color">')(scope);
+    var themeGroup =
+        $compile('<meta content="{{theme}}" irc-theme-group>')(scope);
+    var themeColor = $compile('<meta name="theme-color">')(scope);
+    var notThemeColor = $compile('<meta name="not-theme-color">')(scope);
     head.append(themeGroup);
     head.append(themeColor);
     head.append(notThemeColor);
-
-    scope.onThemeGroupRemove = function() {};
-    spyOn(scope, 'onThemeGroupRemove');
-    themeGroup.bind('$destroy', scope.onThemeGroupRemove);
-
-    scope.onThemeColorRemove = function() {};
-    spyOn(scope, 'onThemeColorRemove');
-    themeColor.bind('$destroy', scope.onThemeColorRemove);
-
-    scope.onNotThemeColorRemove = function() {};
-    spyOn(scope, 'onNotThemeColorRemove');
-    notThemeColor.bind('$destroy', scope.onNotThemeColorRemove);
+    onThemeGroupRemove = jasmine.createSpy();
+    onThemeColorRemove = jasmine.createSpy();
+    onNotThemeColorRemove = jasmine.createSpy();
+    themeGroup.bind('$destroy', onThemeGroupRemove);
+    themeColor.bind('$destroy', onThemeColorRemove);
+    notThemeColor.bind('$destroy', onNotThemeColorRemove);
   }));
 
   it('should remove the theme group element as content changes', function() {
-    expect(scope.onThemeGroupRemove).not.toHaveBeenCalled();
+    expect(onThemeGroupRemove).not.toHaveBeenCalled();
     updateTheme();
-    expect(scope.onThemeGroupRemove).toHaveBeenCalled();
+    expect(onThemeGroupRemove).toHaveBeenCalled();
   });
 
   it('should remove the theme color element as content changes', function() {
-    expect(scope.onThemeColorRemove).not.toHaveBeenCalled();
+    expect(onThemeColorRemove).not.toHaveBeenCalled();
     updateTheme();
-    expect(scope.onThemeColorRemove).toHaveBeenCalled();
+    expect(onThemeColorRemove).toHaveBeenCalled();
   });
 
   it('should not remove other elements as content changes', function() {
     updateTheme();
-    expect(scope.onNotThemeColorRemove).not.toHaveBeenCalled();
+    expect(onNotThemeColorRemove).not.toHaveBeenCalled();
   });
 
   it('should append the elements again', function() {
@@ -72,8 +67,7 @@ describe('irc-action', function() {
 
   beforeEach(inject(function($compile, $rootScope) {
     scope = $rootScope.$new();
-    scope.fun = function() {};
-    spyOn(scope, 'fun');
+    scope.fun = jasmine.createSpy();
     element = $compile('<div irc-action="fun()"></div>')(scope);
   }));
 
