ad2e7ffcbbb10ec812e000c41946ccbca67fff09
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2015-09-19 15:46:43 +0200

    add show / hide button to password field

--- app/js/directives.js
+++ app/js/directives.js
@@ -179,6 +179,47 @@ adapters.directive('ircTextInput', ['$compile', function($compile) {
   };
 }]);
 
+// Add a show/hide button to a gaia-text-input.
+adapters.directive('ircPassword', ['$compile', function($compile) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs) {
+      var show = false;
+      function onShowButton() {
+        var start = input[0].selectionStart;
+        var end = input[0].selectionEnd;
+        show = !show;
+        if (show) {
+          showButton.text('hide');
+          attrs.$set('type', 'text');
+        } else {
+          showButton.text('show');
+          attrs.$set('type', 'password');
+        }
+        input[0].focus();
+        input[0].setSelectionRange(start, end);
+      }
+      var input = angular.element(element[0].els.input);
+      attrs.$set('type', 'password');
+      var showButton = angular.element('<button>show</button>');
+      showButton.css({
+        'position': 'absolute',
+        'right': '1rem',
+        'top': '0px',
+        'height': '40px',
+        'justify-content': 'center',
+        'color': 'var(--highlight-color)',
+        'font-style': 'italic',
+        'font-size': '14px',
+        'background': 'none',
+      });
+      input.css('padding-right', '4rem');
+      input.after(showButton);
+      showButton.on('click', onShowButton);
+    }
+  };
+}]);
+
 // Bind a gaia-checkbox to a model with the "model" attribute.
 adapters.directive('ircCheckbox', ['$parse', function($parse) {
   return {
