9d2018c6cc430a5dcb50c446a644ed5806e66a42
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2015-09-20 19:43:44 +0200

    restore focus in irc-password directive

--- app/views/config/config.js
+++ app/views/config/config.js
@@ -5,41 +5,62 @@ var config = angular.module('irc.views.config', []);
 
 // Add a show/hide button to a gaia-text-input.
 config.directive('ircPassword', [function() {
+  function link(scope, element, attrs) {
+
+    var show = false;
+    var focused = false;
+    var input = angular.element(element[0].els.input);
+    var showButton = angular.element('<button>show</button>');
+
+    // Add button to the DOM.
+    input.after(showButton);
+
+    // Initialize to 'hide'.
+    attrs.$set('type', 'password');
+
+    showButton.css({
+      'position': 'absolute',
+      'right': '1rem',
+      'top': '0px',
+      'height': '40px',
+      'color': 'var(--highlight-color)',
+      'font-style': 'italic',
+      'font-size': '14px',
+      'background': 'none',
+    });
+
+    input.css('padding-right', '4rem');
+
+    showButton.on('click', function() {
+      show = !show;
+      var start = input[0].selectionStart;
+      var end = input[0].selectionEnd;
+      if (show) {
+        showButton.text('hide');
+        attrs.$set('type', 'text');
+      } else {
+        showButton.text('show');
+        attrs.$set('type', 'password');
+      }
+      if (focused) {
+        input[0].focus();
+      }
+      // Put cursor back where it was.
+      input[0].setSelectionRange(start, end);
+    });
+
+    // Set `focused` to true just a moment after input had the focus, so it can
+    // be restored after the button is clicked.
+    input.on('blur', function() {
+      focused = true;
+      setTimeout(function() {
+        focused = false;
+      }, 200);
+    });
+  }
+
   return {
     restrict: 'A',
-    link: function(scope, element, attrs) {
-      var show = false;
-      function onShowButton() {
-        var start = input[0].selectionStart;
-        var end = input[0].selectionEnd;
-        show = !show;
-        if (show) {
-          showButton.text('hide');
-          attrs.$set('type', 'text');
-        } else {
-          showButton.text('show');
-          attrs.$set('type', 'password');
-        }
-        input[0].focus();
-        input[0].setSelectionRange(start, end);
-      }
-      var input = angular.element(element[0].els.input);
-      attrs.$set('type', 'password');
-      var showButton = angular.element('<button>show</button>');
-      showButton.css({
-        'position': 'absolute',
-        'right': '1rem',
-        'top': '0px',
-        'height': '40px',
-        'justify-content': 'center',
-        'color': 'var(--highlight-color)',
-        'font-style': 'italic',
-        'font-size': '14px',
-        'background': 'none',
-      });
-      input.css('padding-right', '4rem');
-      input.after(showButton);
-      showButton.on('click', onShowButton);
-    }
+    link: link
   };
 }]);
