72ec1eb1a87597878c7b337155fceaff5be8006e
Christopher Lübbemeier <christopher.luebbemeier@gmail.com>
2015-09-21 15:41:07 +0200

    disable debug info + strict di

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
 'use strict';
 
-angular.module('irc', [
+var irc = angular.module('irc', [
   'ngAnimate',
   'irc.views',
 ]);
+
+irc.config(['$compileProvider', function($compileProvider) {
+  $compileProvider.debugInfoEnabled(false);
+}]);
